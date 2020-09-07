e39d589c9f0472c6146bb40212ac00749f5a623c
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-07 10:44:52 +0600

    TaskController created

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello')
+// Function to control task data
+const TaskController = (function() {
+    const data = {
+        tasks: [
+            {
+                id: 0,
+                title: 'task1',
+                completed: false
+            },
+            {
+                id: 0,
+                title: 'task1',
+                completed: false
+            }
+        ]
+    };
+
+    return {
+        getTasks(){
+            return data.tasks;
+        }
+    }
+})()
